e6439ab93b5361151ad428f0cb4469cc266a7102
Jan Šilhan <silhanu@gmail.com>
2020-04-29 23:40:29 +0200

    fix: Next driver.

---
+++ packages/driver-next/tests/NextDriver.test.ts
@@ -0,0 +1,117 @@
+import { mockTool, stubExecResult } from '@beemo/core/lib/testUtils';
+import fs from 'fs';
+import NextDriver from '../src/NextDriver';
+
+describe('NextDriver', () => {
+  let driver: NextDriver;
+  // let context: DriverContext;
+  let writeSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    driver = new NextDriver();
+    driver.tool = mockTool();
+    driver.bootstrap();
+
+    // context = stubDriverContext(driver);
+
+    writeSpy = jest.spyOn(fs, 'writeFileSync').mockImplementation(() => true);
+  });
+
+  afterEach(() => {
+    writeSpy.mockRestore();
+  });
+
+  it('sets options from constructor', () => {
+    driver = new NextDriver({
+      args: ['--foo', '--bar=1'],
+      dependencies: ['babel'],
+      env: { DEV: 'true' },
+    });
+
+    expect(driver.options).toEqual({
+      args: ['--foo', '--bar=1'],
+      dependencies: ['babel'],
+      env: { DEV: 'true' },
+      strategy: 'native',
+    });
+  });
+
+  it('sets correct metadata', () => {
+    expect(driver.metadata).toEqual(
+      expect.objectContaining({
+        bin: 'next',
+        configName: 'next.config.js',
+        configOption: '--config',
+        configStrategy: 'reference',
+        dependencies: [],
+        description: 'nextDescription',
+        filterOptions: true,
+        helpOption: '--help',
+        title: 'Next.js',
+        useConfigOption: false,
+        versionOption: '--version',
+        watchOptions: [],
+        workspaceStrategy: 'reference',
+      }),
+    );
+  });
+
+  describe('mergeConfig()', () => {
+    it('merges using eslint engine', () => {
+      expect(
+        driver.mergeConfig(
+          {
+            env: {
+              node: true,
+            },
+            rules: {
+              foo: 'error',
+            },
+          },
+          {
+            rules: {
+              foo: ['error', 'always'],
+            },
+          },
+        ),
+      ).toEqual({
+        env: {
+          node: true,
+        },
+        rules: {
+          foo: ['error', 'always'],
+        },
+      });
+    });
+
+    it('merges ignore list correctly', () => {
+      expect(
+        driver.mergeConfig(
+          {
+            ignore: ['foo', 'bar'],
+          },
+          {
+            ignore: ['baz', 'foo'],
+          },
+        ),
+      ).toEqual({
+        ignore: ['foo', 'bar', 'baz'],
+      });
+    });
+  });
+
+  describe('processFailure()', () => {
+    it('outputs stderr and stdout', () => {
+      const errorSpy = jest.spyOn(driver.tool.console, 'logError');
+
+      driver.processFailure(
+        stubExecResult({
+          command: 'next',
+          stderr: 'Error',
+        }),
+      );
+
+      expect(errorSpy).toHaveBeenCalledWith('Error');
+    });
+  });
+});
