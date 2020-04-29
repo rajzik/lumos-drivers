12f9014518cd6b95c440826f503fb9ba85ed996b
Jan Šilhan <silhanu@gmail.com>
2020-04-30 00:31:26 +0200

    types: Add next config types.

--- packages/driver-next/src/types.ts
+++ packages/driver-next/src/types.ts
@@ -1,2 +1,76 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface NextDriverConfig {}
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_EXPORT,
+  PHASE_PRODUCTION_BUILD,
+  PHASE_PRODUCTION_SERVER,
+} from 'next/constants';
+
+export type NextPhases =
+  | typeof PHASE_DEVELOPMENT_SERVER
+  | typeof PHASE_EXPORT
+  | typeof PHASE_PRODUCTION_BUILD
+  | typeof PHASE_PRODUCTION_SERVER;
+
+export interface NextConfigObject {
+  env: {
+    [key: string]: string | number | boolean;
+  };
+  webpack: Nullable<unknown>;
+  webpackDevMiddleware: Nullable<unknown>;
+  distDir: string;
+  assetPrefix: string;
+  configOrigin: string;
+  useFileSystemPublicRoutes: boolean;
+  generateBuildId(): void;
+  generateEtags: boolean;
+  pageExtensions: string[];
+  target: 'server' | 'serverless';
+  poweredByHeader: boolean;
+  compress: boolean;
+  devIndicators: {
+    buildActivity: boolean;
+    autoPrerender: boolean;
+  };
+  onDemandEntries: {
+    maxInactiveAge: number;
+    pagesBufferLength: number;
+  };
+  amp: { canonicalBase: string };
+  exportTrailingSlash: boolean;
+  experimental: {
+    cpus: number;
+    jsconfigPaths: boolean;
+    css: boolean;
+    scss: boolean;
+    documentMiddleware: boolean;
+    granularChunks: boolean;
+    modern: boolean;
+    plugins: boolean;
+    profiling: boolean;
+    sprFlushToDisk: boolean;
+    reactMode: string;
+    workerThreads: boolean;
+    basePath: string;
+    sassOptions: {
+      [key: string]: unknown;
+    };
+    pageEnv: boolean;
+    measureFid: boolean;
+  };
+  future: { excludeDefaultMomentLocales: boolean };
+  serverRuntimeConfig: {
+    [key: string]: unknown;
+  };
+  publicRuntimeConfig: {
+    [key: string]: unknown;
+  };
+  reactStrictMode: boolean;
+}
+
+export interface NextDefaultConfig {
+  defaultConfig: NextConfigObject;
+}
+
+export type NextDriverConfig =
+  | NextConfigObject
+  | ((phase: NextPhases, defaultConfig: NextDefaultConfig) => NextConfigObject);
