50f678343647d3b49cbe0522df2d98ed8072cc2d
Jan Šilhan <silhanu@gmail.com>
2020-04-30 00:49:44 +0200

    types: Add next driver types.

--- packages/driver-next/src/types.ts
+++ packages/driver-next/src/types.ts
@@ -4,6 +4,7 @@ import {
   PHASE_PRODUCTION_BUILD,
   PHASE_PRODUCTION_SERVER,
 } from 'next/constants';
+import webpack from 'webpack';
 
 export type NextPhases =
   | typeof PHASE_DEVELOPMENT_SERVER
@@ -12,59 +13,88 @@ export type NextPhases =
   | typeof PHASE_PRODUCTION_SERVER;
 
 export interface NextConfigObject {
-  env: {
+  env?: {
     [key: string]: string | number | boolean;
   };
-  webpack: Nullable<unknown>;
-  webpackDevMiddleware: Nullable<unknown>;
-  distDir: string;
-  assetPrefix: string;
-  configOrigin: string;
-  useFileSystemPublicRoutes: boolean;
-  generateBuildId(): void;
-  generateEtags: boolean;
-  pageExtensions: string[];
-  target: 'server' | 'serverless';
-  poweredByHeader: boolean;
-  compress: boolean;
-  devIndicators: {
-    buildActivity: boolean;
-    autoPrerender: boolean;
+  webpack?: Nullable<
+    (config: webpack.Configuration, options: WebpackOptions) => webpack.Configuration
+  >;
+  webpackDevMiddleware?: Nullable<(config: webpack.Configuration) => webpack.Configuration>;
+  distDir?: string;
+  assetPrefix?: string;
+  configOrigin?: string;
+  useFileSystemPublicRoutes?: boolean;
+  generateBuildId?(): Promise<string>;
+  generateEtags?: boolean;
+  pageExtensions?: string[];
+  target?: 'server' | 'serverless';
+  poweredByHeader?: boolean;
+  compress?: boolean;
+  devIndicators?: {
+    buildActivity?: boolean;
+    autoPrerender?: boolean;
   };
-  onDemandEntries: {
-    maxInactiveAge: number;
-    pagesBufferLength: number;
+  onDemandEntries?: {
+    maxInactiveAge?: number;
+    pagesBufferLength?: number;
   };
-  amp: { canonicalBase: string };
-  exportTrailingSlash: boolean;
-  experimental: {
-    cpus: number;
-    jsconfigPaths: boolean;
-    css: boolean;
-    scss: boolean;
-    documentMiddleware: boolean;
-    granularChunks: boolean;
-    modern: boolean;
-    plugins: boolean;
-    profiling: boolean;
-    sprFlushToDisk: boolean;
-    reactMode: string;
-    workerThreads: boolean;
-    basePath: string;
-    sassOptions: {
+  amp?: { canonicalBase?: string; [key: string]: unknown };
+  exportTrailingSlash?: boolean;
+  experimental?: {
+    cpus?: number;
+    jsconfigPaths?: boolean;
+    css?: boolean;
+    scss?: boolean;
+    documentMiddleware?: boolean;
+    granularChunks?: boolean;
+    modern?: boolean;
+    plugins?: boolean;
+    profiling?: boolean;
+    sprFlushToDisk?: boolean;
+    reactMode?: string;
+    workerThreads?: boolean;
+    basePath?: string;
+    sassOptions?: {
       [key: string]: unknown;
     };
-    pageEnv: boolean;
-    measureFid: boolean;
+    pageEnv?: boolean;
+    measureFid?: boolean;
   };
-  future: { excludeDefaultMomentLocales: boolean };
-  serverRuntimeConfig: {
+  future?: { excludeDefaultMomentLocales?: boolean };
+  serverRuntimeConfig?: {
     [key: string]: unknown;
   };
-  publicRuntimeConfig: {
+  publicRuntimeConfig?: {
     [key: string]: unknown;
   };
-  reactStrictMode: boolean;
+  reactStrictMode?: boolean;
+  typescript?: {
+    [key: string]: unknown;
+  };
+  exportPathMap?(defaultPathMap: PathMap, options: PathMapOptions): Promise<PathMap>;
+}
+
+export interface WebpackOptions {
+  buildId: string;
+  dev: boolean;
+  isServer: boolean;
+  defaultLoaders: webpack.Loader[];
+  webpack: unknown;
+}
+
+export interface PathMapOptions {
+  dev: boolean;
+  dir: string;
+  outDir: Nullable<string>;
+  distDir: string;
+  buildId: string;
+}
+
+export interface PathMap {
+  page: string;
+  query: {
+    [key: string]: unknown;
+  };
 }
 
 export interface NextDefaultConfig {
